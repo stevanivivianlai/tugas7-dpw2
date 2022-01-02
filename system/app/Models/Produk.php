d72df4d2c87f09db30ac674a78d6237a55ea7289
stevanivivianlai <stevanivivianlai123@gmail.com>
2022-01-02 15:26:07 +0700

    Initial commit

---
+++ system/app/Models/Produk.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Models;
+
+class produk extends Model{
+	protected $table = 'produk';
+
+
+	function seller(){
+		return $this->belongsTo(User::class, 'id_user');
+	}
+}
---
+++ system/resources/views/admin/produk/show.blade.php
@@ -0,0 +1,32 @@
+@extends("admin.base")
+@section('content')
+<!-- start coding -->
+
+   
+   <div class="container">
+   	<div class="card mt-3 pt-3">
+   	<div class="row ml-1 mr-2 mb-3">
+   		<h4>Detail Data Produk</h4>
+   	</div>
+
+   	<div class="container">
+   		<div class="card-body">
+   			<h3>{{$produk->nama}}</h3>
+            <hr>
+            <p>Rp. {{number_format($produk->harga)}} |
+               Brand : {{$produk->brand}} |
+               Stok : {{$produk->stok}} |
+               Seller : {{$produk->seller->nama}}
+            </p>
+            {!! nl2br ($produk->deskripsi) !!}
+   			</div>	
+   		</div>
+   		
+   	</div>
+
+   	</div>
+   </div>
+
+<!-- end coding -->
+@endsection
+
